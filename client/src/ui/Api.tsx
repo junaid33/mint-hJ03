4ece352e659476b0ebeb49ecc3a391de1c7eb403
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-12-08 16:53:35 -0500

    Show anchor beside header

--- client/src/ui/Api.tsx
+++
@@ -1,6 +0,0 @@
-// TODO: Remove and have customers use ApiPlayground or a standardized API component
-import { ApiPlayground } from './ApiPlayground';
-
-const Api = ApiPlayground;
-
-export { Api };
