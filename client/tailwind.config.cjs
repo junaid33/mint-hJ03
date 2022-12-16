d0a4a771246c898c0fefca2118bc5eed079077e3
tobias-mintlify <110702161+tobias-mintlify@users.noreply.github.com>
2022-12-16 10:14:40 -0500

    Delete unused utils

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -291,17 +291,6 @@ module.exports = {
       addVariant('scrollbar-thumb', '&::-webkit-scrollbar-thumb');
     },
     function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          'bg-grid': (value) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
-      );
-
       matchUtilities({ values: flattenColorPalette(theme('backgroundColor')), type: 'color' });
     },
   ],
