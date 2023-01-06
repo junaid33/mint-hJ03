12f6b0e7088dabc5c4679ef10d4f1602d14e5133
Tobias <110702161+tobias-mintlify@users.noreply.github.com>
2023-01-06 12:52:34 -0500

    Bump dependencies (#133)
    * Bump dependencies
    
    * Allowlist button

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -105,6 +105,7 @@ export const allowedComponents = [
   'link',
   'svg',
   'path',
+  'button',
   // Custom tags
   'zapier-zap-templates',
 ];
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -240,7 +240,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     function ({ addVariant }) {
