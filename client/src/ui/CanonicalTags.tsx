2a0edb287520fa4f9fc80b1ac72a42e0fe62c6d3
Tobias <110702161+tobias-mintlify@users.noreply.github.com>
2023-02-10 14:16:45 -0800

    Set base path in canonical URLs (#352)

--- client/src/ui/CanonicalTags.tsx
+++ client/src/ui/CanonicalTags.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 
 import { useCurrentPath } from '@/hooks/useCurrentPath';
 
 export const CanonicalTags = () => {
+  const router = useRouter();
   const [origin, setOrigin] = useState('');
 
   useEffect(() => {
@@ -12,7 +14,8 @@ export const CanonicalTags = () => {
 
   // useCurrentPath needs to be nested inside ConfigContext so we can't calculate the
   // canonical url in SupremePageLayout.
-  const currentUrl = origin + useCurrentPath();
+  const currentUrl = origin + router.basePath + useCurrentPath();
+
   return (
     <Head>
       <meta key="og:url" name="og:url" content={currentUrl} />
