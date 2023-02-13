46aa5a37246f3747f1fcaa0e7a18814a5918cbd2
Tobias <110702161+tobias-mintlify@users.noreply.github.com>
2023-02-12 17:55:51 -0800

    Use startsWith instead of includes (#356)

--- client/src/types/config.ts
+++ client/src/types/config.ts
@@ -124,7 +124,7 @@ export const findFirstNavigationEntry = (
 ): NavigationEntry | undefined => {
   return group.pages.find((page) => {
     if (typeof page === 'string') {
-      return page.includes(target);
+      return page.startsWith(target);
     } else {
       return findFirstNavigationEntry(page, target);
     }
