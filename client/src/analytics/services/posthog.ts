d003f7db5d90075be591ac6bfb50ce93060a6d37
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-02-24 00:01:29 -0500

    Remove default posthog pageview

--- client/src/analytics/services/posthog.ts
+++ client/src/analytics/services/posthog.ts
@@ -14,6 +14,7 @@ export default class PostHogAnalytics extends AnalyticsService {
     // apiHost only has to be passed in if the user is self-hosting PostHog
     posthog.init(implementationConfig.apiKey, {
       api_host: implementationConfig.apiHost || 'https://app.posthog.com',
+      capture_pageview: false, // disable default pageview
       loaded: (posthogInstance) => {
         if (process.env.NODE_ENV !== 'production') posthogInstance.opt_out_capturing();
       },
